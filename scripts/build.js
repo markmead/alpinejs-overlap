ce6945472df6637fd4a2b142c08658fbc841ce0b
Mark Mead <itsmarkmead@gmail.com>
2022-12-10 10:32:01 +0000

    Update qoutes

--- scripts/build.js
+++ scripts/build.js
@@ -1,11 +1,11 @@
 buildPlugin({
-  entryPoints: [`builds/cdn.js`],
+  entryPoints: ['builds/cdn.js'],
   outfile: 'dist/overlap.min.js',
 })
 
 buildPlugin({
-  entryPoints: [`builds/module.js`],
-  outfile: `dist/overlap.esm.js`,
+  entryPoints: ['builds/module.js'],
+  outfile: 'dist/overlap.esm.js',
   platform: 'neutral',
   mainFields: ['main', 'module'],
 })
